c317e1b33df2fdc536840acce429c294a6b70fd5
kindaboringkvg <karam.veer00@gmail.com>
2024-08-19 19:19:21 +0530

    making a comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,8 +1,87 @@
-const Comment = () => {
+"use client"
+
+import{ useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import * as z from "zod"
+import { Input } from "@/components/ui/input"
+import { usePathname, useRouter } from "next/navigation";
+
+// import { updateUser } from "@/lib/actions/user.actions";
+import { CommentValidation } from "@/lib/validations/rumour";
+import { createRumour } from "@/lib/actions/rumour.actions";
+
+interface Props {
+    rumourId: string;
+    currentUserImg: string;
+    currentUserId: string;
+}
+
+const Comment = ({rumourId, currentUserImg, currentUserId}: Props) => {
+    const router = useRouter();
+    const pathname = usePathname();
+    
+    const form = useForm({
+        resolver: zodResolver(CommentValidation),
+        defaultValues: {
+            rumour: '',
+        }
+    })
+
+    const onSubmit = async (values : z.infer<typeof CommentValidation>) => {
+    //   await createRumour({
+    //     text : values.rumour,
+    //     author : userId,
+    //     communityId : null,
+    //     path : pathname,
+
+    //   });
+
+      router.push("/");
+    };
+    
     return (
-        <div>
-            <h1 className="text-white">Comment Form</h1>
-        </div>
+        <Form {...form}>
+      <form 
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="flex flex-col justify-start gap-10">
+
+
+        <FormField
+          control={form.control}
+          name="rumour"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
+                fixing here
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3  text-light-1">
+                <Input 
+                type = "text"
+                placeholder="Comment..."
+                className="no-focus text-light-1 outline-none"
+                {...field}
+                />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}
+        />
+
+        <Button type = "submit" className="bg-primary-500">
+          Spread Rumour
+        </Button>
+      </form>
+      </Form>
     )
 }
 
